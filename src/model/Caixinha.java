4663902cee93c8eb95a3b8f87744b7f187f06e21
MarcosJr1 <marcosjr.vcs@gmail.com>
2019-10-02 22:27:20 -0300

    Classes adicionais

---
+++ src/model/Caixinha.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Caixinha {
+    public boolean mesclada;
+    public int valor;
+
+    public Caixinha(int val) {
+        valor = val;
+    }
+
+    public int pegaValor() {
+        return valor;
+    }
+
+    public void setMesclada(boolean m) {
+        mesclada = m;
+    }
+
+    public boolean podeMesclarCom(Caixinha proxima) {
+        if(!mesclada && proxima != null && !proxima.mesclada && valor == proxima.pegaValor()){
+            return true;
+        } 
+        else {
+            return false;
+        }
+    }
+
+    public int mesclarCom(Caixinha proxima) {
+        if (podeMesclarCom(proxima)) {
+            valor *= 2;
+            mesclada = true;
+            return valor;
+        }
+        return -1;
+    }
+}
